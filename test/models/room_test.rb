229baf91914703814f3093a4ad0b467a3d2fbd04
camilopineda77 <camilopineda77@hotmail.com>
2018-09-05 22:40:57 -0500

    adding avaliable rooms return test

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -4,4 +4,14 @@ class RoomTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "method avaliables should return only avaliable rooms" do
+    rooms = Room.create( [ { number: 101, floor: 1, bed_number: 2, guests_number: 3, avaliable: true },
+                           { number: 102, floor: 1, bed_number: 1, guests_number: 2, avaliable: true },
+                           { number: 103, floor: 1, bed_number: 3, guests_number: 4, avaliable: false }
+                        ] )
+
+    avaliablesRooms = Room.avaliables
+    expectedRooms = [ Room.find_by(number: 101), Room.find_by(number: 102) ]
+    assert_equal(expectedRooms, avaliablesRooms)
+  end
 end
