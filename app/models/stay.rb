7ef40613b3f043a57682deb7b484d2d1144bc02c
camilopineda77 <camilopineda77@hotmail.com>
2018-09-06 00:16:39 -0500

    adding test and validations to room avaliability

--- app/models/stay.rb
+++ app/models/stay.rb
@@ -5,10 +5,17 @@ class Stay < ApplicationRecord
   accepts_nested_attributes_for :room
   validates :date_in, :date_out, :guest, :room,  presence: true
   validate :date_out_greater_than_date_in
+  validate :room_avaliability
 
   def date_out_greater_than_date_in
-    if date_in >= date_out
+    if date_in.present? && date_out.present? && date_in >= date_out
       errors.add(:date_in, "date in can't be greater than date out")
+    end
+  end
+
+  def room_avaliability
+    if room.present? && room.avaliable == false
+      errors.add(:room, "room no avaliables")
     end
   end
 end
--- test/models/stay_test.rb
+++ test/models/stay_test.rb
@@ -5,7 +5,7 @@ class StayTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "should not save a stay without room, guest, date_in and date_out" do
-    stay = Stay.new(date_in: "12/12/2018", date_out: "12/12/2018")
+    stay = Stay.new(date_in: "12/12/2018")
     assert_not stay.save
   end
 
@@ -32,6 +32,28 @@ class StayTest < ActiveSupport::TestCase
   end
 
   test "should save a stay with date_out greater or equal than date_in" do
+    room= Room.create( number: 101, floor: 1, bed_number: 2,
+                       guests_number: 3, avaliable: true )
+    membership = Membership.create( nombre: "oro", discount: 0.1 )
+    guest = Guest.create( nombre: "sebastian", apellido: "pineda",
+                          documento: 12345, membership: Membership.find_by( nombre: "oro" ) )
+
+    stay = Stay.new(date_in: "12/12/2018", date_out: "13/12/2018",room: room, guest: guest )
+    assert stay.save
+  end
+
+  test "should not save a stay with a room no avaliable" do
+    room= Room.create( number: 101, floor: 1, bed_number: 2,
+                       guests_number: 3, avaliable: false )
+    membership = Membership.create( nombre: "oro", discount: 0.1 )
+    guest = Guest.create( nombre: "sebastian", apellido: "pineda",
+                          documento: 12345, membership: Membership.find_by( nombre: "oro" ) )
+
+    stay = Stay.new(date_in: "12/12/2018", date_out: "13/12/2018",room: room, guest: guest )
+    assert_not stay.save
+  end
+
+  test "should save a stay with a room avaliable" do
     room= Room.create( number: 101, floor: 1, bed_number: 2,
                        guests_number: 3, avaliable: true )
     membership = Membership.create( nombre: "oro", discount: 0.1 )
